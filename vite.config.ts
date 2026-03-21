e88340533687384424ba3334264bf294357c2b45
Sergey <sergejmatvejcuk323@gmail.com>
2026-03-21 21:31:09 +0300

    fix: simplify vite config for standalone deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,15 @@
 import { defineConfig } from 'vite'
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin'
 import { resolve } from 'path'
 
 export default defineConfig({
-	root: resolve(process.cwd(), 'pet-spa/src'),
-	cacheDir: '../node_modules/.vite/pet-spa',
-	server: {
-		port: 4200,
-		host: 'localhost'
-	},
-	preview: {
-		port: 4300,
-		host: 'localhost'
-	},
-	plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+	root: resolve(process.cwd(), 'src'),
 	build: {
 		outDir: resolve(process.cwd(), 'public'),
 		emptyOutDir: true,
-		reportCompressedSize: true,
-		commonjsOptions: {
-			transformMixedEsModules: true
-		}
+		reportCompressedSize: true
+	},
+	server: {
+		port: 4200,
+		host: 'localhost'
 	}
 })
